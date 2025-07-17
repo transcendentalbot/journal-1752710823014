b0fddfcb58b9078bd8d1b60470d364bd78466df7
transcendentalbot <admin@transcendentalbot.net>
2025-07-16 20:07:15 -0400

    Add components/Home.tsx for Develop: Basic Landing Page

---
+++ components/Home.tsx
@@ -0,0 +1,11 @@
+import Navigation from '@/components/Navigation';
+import HeroSection from '@/components/HeroSection';
+
+export default function Home() {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+}
